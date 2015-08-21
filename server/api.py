c6ef6fb0f969ad96396ad8011615619c4960050f
Rafael Campos <rcampos@mac-rcampos.local>
2015-08-21 18:50:17 -0300

    Working on the endpoint; Implemented: - Retrieve shops by coordinates and radius - Retrieve products by shops - Order products by popularity - Limit products by count

--- server/api.py
+++ server/api.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-
-from flask import Blueprint, current_app, jsonify
+from __future__ import unicode_literals
+from flask import Blueprint, current_app, jsonify, request
+from server.haversine import haversine_in_meters
+from server.models import from_csv, Shop, Product
+import math
 
 
 api = Blueprint('api', __name__)
@@ -11,6 +14,52 @@ def data_path(filename):
     return u"%s/%s" % (data_path, filename)
 
 
+
+#     "count": "10", 
+#     "lat": "59.33258", 
+#     "lng": "18.0649", 
+#     "radius": "500"
+
+
+def shop_in_radius(row, args):
+    dist = haversine_in_meters(
+        float(args['lng']),
+        float(args['lat']),
+        float(row[Shop.LNG]),
+        float(row[Shop.LAT]))
+    
+    if dist < float(args['radius']):
+        return True
+    else:
+        return False
+
+
+def products_in_shops(row, shops):
+    shops_ids = [shop['id'] for shop in shops] 
+    
+    if row[Product.SHOP_ID] in shops_ids:
+        return True
+    else:
+        return False
+    
 @api.route('/search', methods=['GET'])
 def search():
-    return jsonify({'products': []})
+    shops = from_csv('shops', shop_in_radius, request.args)
+    products = from_csv('products', products_in_shops, shops)
+    
+    """sorts by popularity"""
+    products = sorted(products, key=lambda k: k['popularity'], reverse=True)
+    
+    """limits the results"""
+    products = products[:int(request.args['count'])]
+    
+    #TODO filter tags
+    #TODO Unicode API?
+    #TODO limit Access-Control-Allow-Origin??
+
+#     response = jsonify({ 'shops': shops })
+    response = jsonify({ 'products': products })
+    
+    """Allow access from any other host for now - later we discuss security for this"""
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
---
+++ server/models.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+'''
+Created on Aug 20, 2015
+
+@author: rcampos
+'''
+from __future__ import unicode_literals
+import csv
+
+class CsvRow:
+    ID = 0
+    
+class Product(CsvRow):
+    SHOP_ID=1
+    TITLE=2
+    POPULARITY=3
+    QUANTITY=4
+    
+class Shop(CsvRow):
+    NAME=1
+    LAT=2
+    LNG=3
+    
+class Tag(CsvRow):
+    TAG=1
+    
+class Tagging(CsvRow):
+    SHOP_ID=1
+    TAG_ID=2
+    
+
+data_path = 'data/'
+def from_csv(name, cbfilter=None, args=None):
+    with open(data_path+name+'.csv', 'r') as f:
+        reader = csv.reader(f)
+        
+        """Condition (row[0] == 'id') assumes all csvs will have a label row with the first cell as 'id'"""
+        if cbfilter:
+            rows = [row if row[CsvRow.ID] == 'id' or cbfilter(row, args) is True else None for row in reader]
+        else:
+            rows = [row for row in reader]
+            
+        rows = filter(None, rows)
+
+        labels = rows[:1][0]
+        products = rows[1:]
+         
+        return [dict(zip(labels, prod)) for prod in products]
